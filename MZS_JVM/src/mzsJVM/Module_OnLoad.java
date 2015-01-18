a2633b2f673e5ad0e6dcdb5472882211f68233ec
zunath <coolty3001@yahoo.com>
2015-01-18 15:14:55 -0600

    Added NWNX.APS.java

--- MZS_JVM/src/mzsJVM/Module_OnLoad.java
+++ MZS_JVM/src/mzsJVM/Module_OnLoad.java
@@ -1,5 +1,6 @@
 package mzsJVM;
 
+import mzsJVM.NWNX.APS;
 import org.nwnx.nwnx2.jvm.NWObject;
 import org.nwnx.nwnx2.jvm.NWScript;
 import org.nwnx.nwnx2.jvm.SchedulerListener;
@@ -10,6 +11,8 @@ public class Module_OnLoad implements SchedulerListener {
 	public void context(NWObject objSelf) {}
 	
 	public void event(NWObject objSelf, String event) {
-		NWScript.executeScript("aps_onload", objSelf);
+		APS.SQLInitialize();
+
+
 	}
 }
