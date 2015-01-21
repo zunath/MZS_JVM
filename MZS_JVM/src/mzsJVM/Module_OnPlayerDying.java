44799504ef568f62c057849283f604b22f1d3b96
zunath <coolty3001@yahoo.com>
2015-01-20 20:04:57 -0600

    Migrated module OnPlayerEquipItem event

--- MZS_JVM/src/mzsJVM/Module_OnPlayerDying.java
+++ MZS_JVM/src/mzsJVM/Module_OnPlayerDying.java
@@ -1,5 +1,6 @@
 package mzsJVM;
 import org.nwnx.nwnx2.jvm.*;
+import org.nwnx.nwnx2.jvm.constants.*;
 
 public class Module_OnPlayerDying implements IScriptEventHandler {
 	@Override
@@ -8,7 +9,108 @@ public class Module_OnPlayerDying implements IScriptEventHandler {
 		NWObject oDatabase = NWScript.getItemPossessedBy(oPC, Constants.PCDatabaseTag);
 
 		if (NWScript.getLocalInt(oDatabase, "zombified") == 1) return;
-		NWScript.executeScript("bleed_on_dying", objSelf);
+		BleedProcess(objSelf);
 
 	}
+
+	private void BleedProcess(final NWObject objSelf) {
+		NWObject oDying = NWScript.getLastPlayerDying();
+		NWScript.setLocalObject(oDying, "PCName", oDying);
+		NWScript.setLocalInt(oDying, "iPCDCC", 0);
+
+		Scheduler.assign(oDying, new Runnable() {
+			@Override
+			public void run() {
+				NWScript.clearAllActions(false);
+				Bleed(objSelf, 1);
+			}
+		});
+
+	}
+
+
+	private void Bleed(final NWObject objSelf, int iBleedAmt)
+	{
+		NWObject oDying = NWScript.getLocalObject(objSelf, "PCName");
+		//SendMessageToPC(oDying, "Death status: " + IntToString(GetLocalInt(GetItemPossessedBy(oDying, "database"), "DEAD"))); // DEBUG
+		int iDCC = NWScript.getLocalInt(oDying, "iPCDCC");
+		iDCC = iDCC + 1;
+		int iDC;
+		int iDCBase = 16; /* Decrease/increase this value to lower/raise the base DC. */
+		iDC = iDCBase;
+
+		if(iDCC > iDCBase)
+		{
+			iDC = iDCC; /* This is used to decrease the PC's chance to stabilize
+                        should they not stabilize after iDCBase turns.
+                        Is really only effective when iDCBase < 10*/
+		}
+
+		NWEffect eBleedEff;
+
+    /* keep executing recursively until character is dead or at +1 hit points */
+		if (NWScript.getCurrentHitPoints(objSelf) <= 0)
+		{
+
+        /* a positive bleeding amount means damage, otherwise heal the character */
+			if (iBleedAmt > 0)
+			{
+				eBleedEff = NWScript.effectDamage(iBleedAmt, DamageType.MAGICAL, DamagePower.NORMAL);
+			}
+			else
+			{
+				eBleedEff = NWScript.effectHeal(-iBleedAmt);  /* note the negative sign */
+			}
+
+			NWScript.applyEffectToObject(Duration.TYPE_INSTANT, eBleedEff, oDying, 0.0f);
+
+        /* -10 hit points is the death threshold, at or beyond it the character dies */
+			if (NWScript.getCurrentHitPoints(objSelf) <= -10)
+			{
+				NWScript.playVoiceChat(VoiceChat.DEATH, objSelf); /* scream one last time */
+				NWScript.applyEffectToObject(Duration.TYPE_INSTANT, NWScript.effectVisualEffect(VfxImp.DEATH, false), objSelf, 0.0f); /* make death dramatic */
+				NWScript.applyEffectToObject(Duration.TYPE_INSTANT, NWScript.effectDeath(false, true), oDying, 0.0f); /* now kill them */
+				return;
+			}
+
+			if (iBleedAmt > 0)  /* only check if character has not stablized */
+			{
+				int iConBonus = NWScript.getAbilityModifier(Ability.CONSTITUTION, oDying);
+				int iStableRoll = NWScript.random(21) + iConBonus;
+				if(iStableRoll >= iDC)
+				{
+					iBleedAmt = -iBleedAmt; /* reverse the bleeding process */
+					NWScript.playVoiceChat(VoiceChat.LAUGH, objSelf); /* laugh at death -- this time */
+					NWScript.sendMessageToPC(oDying, "You have stabilized and have begun to heal.");
+				}
+				else
+				{
+					NWScript.sendMessageToPC(oDying, "You have failed to stabilize in round: " + NWScript.intToString(iDCC) + ".");
+					switch (NWScript.random(6))
+					{
+						case 0: NWScript.playVoiceChat(VoiceChat.PAIN1, objSelf); break;
+						case 1: NWScript.playVoiceChat(VoiceChat.PAIN2, objSelf); break;
+						case 2: NWScript.playVoiceChat(VoiceChat.PAIN3, objSelf); break;
+						case 3: NWScript.playVoiceChat(VoiceChat.HEALME, objSelf); break;
+						case 4: NWScript.playVoiceChat(VoiceChat.NEARDEATH, objSelf); break;
+						case 5: NWScript.playVoiceChat(VoiceChat.HELP, objSelf);
+					}
+				}
+			}
+
+			if (NWScript.getCurrentHitPoints(objSelf) <= 0) {
+				NWScript.setLocalInt(oDying, "iPCDCC", iDC);
+
+				final int nextBleed = iBleedAmt;
+				Scheduler.delay(objSelf, 6000, new Runnable() {
+					@Override
+					public void run() {
+						Bleed(objSelf, nextBleed);
+					}
+				});
+				Scheduler.flushQueues();
+			}
+		}
+	}
+
 }
--- MZS_JVM/src/mzsJVM/Module_OnPlayerEquipItem.java
+++ MZS_JVM/src/mzsJVM/Module_OnPlayerEquipItem.java
@@ -1,9 +1,126 @@
 package mzsJVM;
 import org.nwnx.nwnx2.jvm.*;
+import org.nwnx.nwnx2.jvm.constants.AttackBonus;
+import org.nwnx.nwnx2.jvm.constants.Duration;
+import org.nwnx.nwnx2.jvm.constants.InventorySlot;
 
+@SuppressWarnings("unused")
 public class Module_OnPlayerEquipItem implements IScriptEventHandler {
 	@Override
-	public void runScript(NWObject objSelf) {
+	public void runScript(final NWObject objSelf) {
+
+
+		final NWObject oItem = NWScript.getPCItemLastEquipped();
+		NWObject oPC   = NWScript.getPCItemLastEquippedBy();
+		NWObject oDatabase = NWScript.getItemPossessedBy(oPC, Constants.PCDatabaseTag);
+
+		NWObject oWeapon = NWScript.getItemInSlot(InventorySlot.RIGHTHAND, oPC);
+		NWObject oOffHand = NWScript.getItemInSlot(InventorySlot.LEFTHAND, oPC);
+
+		int iIdentification1;
+		int iIdentification2;
+		int iFireRateMode1 = NWScript.getLocalInt(oWeapon, "FireRateMode1");
+		int iFireRate1 = NWScript.getLocalInt(oWeapon, "FireRate1");
+		int iAccuracyPenalty = 0;
+
+		if (iFireRateMode1 <= 0)
+		{
+			NWScript.setLocalInt(oWeapon, "FireRateMode1", 1);
+		}
+
+		if (iFireRateMode1 == 1)
+		{
+			iAccuracyPenalty = 0;
+			iFireRate1 = NWScript.getLocalInt(oWeapon, "FireRate1");
+		}
+
+		if (iFireRateMode1 == 2)
+		{
+			iFireRate1 = NWScript.getLocalInt(oWeapon, "FireRate2");
+			iAccuracyPenalty = 1;
+		}
+
+		if (iFireRateMode1 == 3)
+		{
+			iFireRate1 = NWScript.getLocalInt(oWeapon, "FireRate3");
+			iAccuracyPenalty = 2;
+		}
+
+		final NWEffect eExtraAttack1 = NWScript.effectModifyAttacks(iFireRate1 - 1);
+		final NWEffect eDualGunAccuracyPenalty1 = NWScript.effectAttackDecrease(iAccuracyPenalty, AttackBonus.ONHAND);
+
+		//Added by Drakaden, flashlights now use up a charge 1 minute after they have been equipped.
+		//Added by Drakaden, if the flashlight only has 1 charge remaining, spawn a Flashlight (No Batteries) and destroy the flashlight.
+		if (NWScript.getTag(oItem).equals("_mdrn_ot_light"))
+		{
+			Scheduler.delay(objSelf, 60000, new Runnable() {
+				@Override
+				public void run() {
+					NWScript.setItemCharges(oItem, NWScript.getItemCharges(oItem) - 1);
+				}
+			});
+
+			if (NWScript.getItemCharges(oItem) <= 1)
+			{
+				NWScript.createItemOnObject("flashlightnob001", oPC, 1, "");
+				NWScript.destroyObject(oItem, 0.0f);
+			}
+
+		}
+
+		//Added by Drakaden, single-weild firearms extra attack.
+		iIdentification1 = NWScript.findSubString(NWScript.getTag(oOffHand), "off", 0);
+		iIdentification2 = NWScript.findSubString(NWScript.getTag(oWeapon), "dual", 0);
+
+		if (NWScript.getWeaponRanged(oItem) && iIdentification1 <= -1 && iIdentification2 <= 1)
+		{
+			if (NWScript.getLocalInt(oDatabase, "DualGunSEffect1") != 1)
+			{
+				Scheduler.assign(oPC, new Runnable() {
+					@Override
+					public void run() {
+						NWScript.applyEffectToObject(Duration.TYPE_TEMPORARY, eExtraAttack1, objSelf, 99999.0f);
+						NWScript.applyEffectToObject(Duration.TYPE_TEMPORARY, eDualGunAccuracyPenalty1, objSelf, 99999.0f);
+					}
+				});
+
+				NWScript.setLocalInt(oDatabase, "DualGunsEffect1", 1);
+			}
+		}
+
+		//Added by Drakaden, dual-weild firearms extra attack.
+		iIdentification1 = NWScript.findSubString(NWScript.getTag(oOffHand), "off", 0);
+		iIdentification2 = NWScript.findSubString(NWScript.getTag(oWeapon), "dual", 0);
+
+		if (iIdentification1 >= 0 && iIdentification2 >= 0)
+		{
+			if (NWScript.getLocalInt(oDatabase, "DualGunSEffect1") != 1)
+			{
+				if (!NWScript.getLocalString(oOffHand, "AmmoType").equals(NWScript.getLocalString(oWeapon, "AmmoType")))
+				{
+					NWScript.sendMessageToPC(oPC, "The main hand and the offhand guns are ammunitions types incompatible, you can still equip them, but the offhand won't do anything.");
+				}
+				else
+				{
+					Scheduler.assign(oPC, new Runnable() {
+						@Override
+						public void run() {
+							NWScript.applyEffectToObject(Duration.TYPE_TEMPORARY, eExtraAttack1, objSelf, 99999.0f);
+							NWScript.applyEffectToObject(Duration.TYPE_TEMPORARY, eDualGunAccuracyPenalty1, objSelf, 99999.0f);
+
+						}
+					});
+
+					NWScript.setLocalInt(oDatabase, "DualGunsEffect1", 1);
+				}
+			}
+		}
+
+		// d20
+		NWScript.executeScript("d20_on_equip", objSelf);
+
+		// Bioware Default
 		NWScript.executeScript("x2_mod_def_equ", objSelf);
+
 	}
 }
